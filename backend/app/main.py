b815e03523b57510ea4e9958ddcfb7b30357ee4c
amith-hasintha <it22167378@my.sliit.lk>
2025-05-16 16:46:33 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "http://localhost:3001",
         "https://shortlist-cv.vercel.app",
         "https://*.vercel.app"
     ],
